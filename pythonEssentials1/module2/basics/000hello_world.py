f7db16c97417d628ada2da013897e9ca14f362e0
Phinehas <walburphinehas78@gmail.com>
2025-05-26 11:23:38 +0300

    Wrote a script that introduces the basic syntax of python including the use of the print() function

---
+++ pythonEssentials1/module2/basics/000hello_world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A simple python script to print 'Hello, World!' to the console.
+
+This script introduces the basic syntax of Python, including the use of the
+print() function.
+It serves as the starting point for learning Python fundamentals.
+"""
+
+print("Hello, World!")
