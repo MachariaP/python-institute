65ce2537b56ad596e50ebc9c8f52b9bf11e0de4d
Phinehas <walburphinehas78@gmail.com>
2025-05-26 11:31:47 +0300

    Used the main() function to execute the 'Hello, World!' program and display a greeting message to the console.

---
+++ pythonEssentials1/module2/basics/001hello_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A simple python script to print 'Hello, World!' to the console.
+
+This script introduces the basic syntax of Python, including the use of the
+print() function.
+It serves as the starting point for learning Python fundamentals.
+"""
+
+def main():
+    """
+    Main function to execute the Hello, WOrld! program.
+
+    Prints a greeting message to the console.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
+
